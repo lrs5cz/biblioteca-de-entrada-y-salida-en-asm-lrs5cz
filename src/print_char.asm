a23d24d656dd13618b1711d85cdaa2c2d2f6dfec
lrs5cz <lrs5cz@gmail.com>
2026-03-15 11:54:16 -0600

    Implement character printing using syscall write

--- src/print_char.asm
+++ src/print_char.asm
@@ -11,14 +11,12 @@ print_char:
 
     push ebp
     mov ebp, esp
-
-    ; TODO:
-    ; 1. Guardar el caracter en memoria
-    ; 2. Usar syscall write
-    ; 3. Imprimir 1 byte
-
-    ; write(fd=1, buffer, 1)
-
+    push eax ; Empujamos eax para que el caracter se encuentre en memoria
+    mov eax, 4 ; syscall: sys_write
+    mov ebx, 1 ; file descriptor: stdout
+    mov ecx, esp ; Puntero al caracter
+    mov edx, 1 ; Longitud: 1 byte
+    int 0x80 ; Llamada al kerneñ 
     mov esp, ebp
     pop ebp
     ret
